7b63e743c486755f1c6b714a880686f77f4651af
cra2ycoder <49195865+cra2ycoder@users.noreply.github.com>
2021-05-20 00:29:48 +0530

    play with http and url

---
+++ src/http.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const url = require("url");
+
+// const HTTP_METHODS = http.METHODS;
+// const HTTP_STATUS_CODES = http.STATUS_CODES;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.statusMessage = "OK";
+
+  // console.log(req.url);
+  const query = url.parse(req.url, true).query;
+  console.log({ query });
+
+  // console.log(req.headers);
+  // console.log(req.method);
+  // console.log(req.rawHeaders);
+  // console.log(res.getHeaderNames());
+  // console.log(res.getHeader())
+  // console.log(res.getHeaders());
+  // res.statusMessage("OK");
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("custom_header", "trail");
+  res.end("Test Message");
+  // res.end(JSON.stringify(HTTP_STATUS_CODES, null, "\t"));
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("server is listening...");
+});
+
+// console.log({ server });
+
+// console.log(HTTP_STATUS_CODES);
+
+// console.log(http.globalAgent);
+
+// http.request();
+
+// http.get();
--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@
 // require("./console");
 // require("./handleInput");
 // require("./modules/calc");
-require("./path");
+// require("./path");
+require("./http");
+
+// const callBack = (a, b) => {
+//   console.log(a + b);
+// };
+
+// setTimeout(callBack, 1000, 10, 10);
